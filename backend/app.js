f0be9d695456107ffa57dcc6ff88812c085448df
dan01jan <danize1001@gmail.com>
2023-10-06 09:57:38 +0800

    middleware

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const cookie = require('cookie-parser')
 
 app.use(express.json());
+app.use(cookie());
 
 const products = require('./routes/product');
 const auth = require('./routes/auth');
-
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {newProduct, getProducts, getSingleProduct, updateProduct,deleteProduct} = require('../controllers/productController');
+const { newProduct, getProducts, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const { isAuthenticatedUser } = require('../middlewares/auth');
 
-
-router.post('/product/new', newProduct);
+router.post('/product/new',isAuthenticatedUser, newProduct);
 router.get('/products', getProducts);
 router.get('/product/:id', getSingleProduct);
-router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
+router.route('/admin/product/:id',isAuthenticatedUser).put(updateProduct).delete(deleteProduct);
 
 
 module.exports = router;
