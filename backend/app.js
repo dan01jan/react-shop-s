c2126b83db68711c3e2ac704cc0659d485ae8f4c
dan01jan <danize1001@gmail.com>
2023-10-12 23:19:21 +0800

    Frontend

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,9 @@ app.use(cookie());
 
 const products = require('./routes/product');
 const auth = require('./routes/auth');
+const order = require('./routes/order');
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
+app.use('/api/v1', order);
 module.exports = app;
