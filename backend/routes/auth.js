e86b10ec40e62ff9946072b9d9a70d3b45171233
dan01jan <danize1001@gmail.com>
2023-10-12 14:22:57 +0800

    Update Password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
-const { registerUser, loginUser, logout, forgotPassword, resetPassword} = require('../controllers/authController');
+const { isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile,
+    updatePassword,
+    updateProfile,} = require('../controllers/authController');
 router.post('/register',registerUser);
 router.post('/login', loginUser);
 router.get('/logout',logout);
 router.post('/password/forgot',forgotPassword);
 router.put('/password/reset/:token',resetPassword);
+router.get('/me', isAuthenticatedUser, getUserProfile)
+router.put('/password/update', isAuthenticatedUser, updatePassword)
 
 // router.get('/logout',logout);
 
