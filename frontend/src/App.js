a5474bc99a04fcf9561f26add8c373ac1b996359
dan01jan <danize1001@gmail.com>
2023-10-13 10:14:18 +0800

    Product Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './Components/Layout/Header'
 import Footer from './Components/Layout/Footer'
 import Home from './Components/Home'
-import { BrowserRouter as Router} from 'react-router-dom';
+import ProductDetails from './Components/Product/ProductDetails'
 
 function App() {
-
   return (
     <div className="App">
-      <Header />
       <Router>
-      <Home/>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} exact="true" />
+          <Route path="/product/:id" element={<ProductDetails />} exact="true" />
+        </Routes>
+        <Footer />
       </Router>
-
-      <Footer />
     </div>
   );
-
 }
 
 export default App;
